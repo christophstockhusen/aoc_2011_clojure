c73f1e05c30b4abdc2e21866a02413cc6672a076
Christoph Stockhusen <mail@christophstockhusen.de>
2022-12-05 16:24:12 +0100

    Readme and core

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Advent of Code 2021 Solutions in Clojure
 
+My first completely solved AoC. :-) 
+
 ## Usage
 
 1. Install [leiningen](https://leiningen.org/)
-1. Run `lein run` in repos base folder
+1. Run `lein test` or `lein run` (this might take some time) 
+in repos base folder.
--- src/aoc_2011/core.clj
+++ src/aoc_2011/core.clj
@@ -1,7 +1,80 @@
-(ns aoc-2011.core)
+(ns aoc-2011.core 
+  (:require [aoc-2011.day-01 :as day-01]
+            [aoc-2011.day-02 :as day-02]
+            [aoc-2011.day-03 :as day-03]
+            [aoc-2011.day-04 :as day-04]
+            [aoc-2011.day-05 :as day-05]
+            [aoc-2011.day-06 :as day-06]
+            [aoc-2011.day-07 :as day-07]
+            [aoc-2011.day-08 :as day-08]
+            [aoc-2011.day-09 :as day-09]
+            [aoc-2011.day-10 :as day-10]
+            [aoc-2011.day-11 :as day-11]
+            [aoc-2011.day-12 :as day-12]
+            [aoc-2011.day-13 :as day-13]
+            [aoc-2011.day-14 :as day-14]
+            [aoc-2011.day-15 :as day-15]
+            [aoc-2011.day-16 :as day-16]
+            [aoc-2011.day-17 :as day-17]
+            [aoc-2011.day-18 :as day-18]
+            [aoc-2011.day-19 :as day-19]
+            [aoc-2011.day-20 :as day-20]
+            [aoc-2011.day-21 :as day-21]
+            [aoc-2011.day-22 :as day-22]
+            [aoc-2011.day-23 :as day-23]
+            [aoc-2011.day-24 :as day-24]
+            [aoc-2011.day-25 :as day-25]))
 
 (defn -main
-  [& args]
-  (print "Hi!"))
+  [& args] 
+  (println [(day-01/a)
+            (day-01/b)
+            (day-02/a)
+            (day-02/b)
+            (day-03/a)
+            (day-03/b)
+            (day-04/a)
+            (day-04/b)
+            (day-05/a)
+            (day-05/b)
+            (day-06/a)
+            (day-06/b)
+            (day-07/a)
+            (day-07/b)
+            (day-08/a)
+            (day-08/b)
+            (day-09/a)
+            (day-09/b)
+            (day-10/a)
+            (day-10/b)
+            (day-11/a)
+            (day-11/b)
+            (day-12/a)
+            (day-12/b)
+            (day-13/a)
+            (day-13/b)
+            (day-14/a)
+            (day-14/b)
+            (day-15/a)
+            (day-15/b)
+            (day-16/a)
+            (day-16/b)
+            (day-17/a)
+            (day-17/b)
+            (day-18/a)
+            (day-18/b)
+            (day-19/a)
+            (day-19/b)
+            (day-20/a)
+            (day-20/b)
+            (day-21/a)
+            (day-21/b)
+            (day-22/a)
+            (day-22/b)
+            (day-23/a)
+            (day-23/b)
+            (day-24/a)
+            (day-24/b)
+            (day-25/a)]))
 
 (-main)
